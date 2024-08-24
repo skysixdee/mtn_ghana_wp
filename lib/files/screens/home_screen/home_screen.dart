287d4a5cc7af94071e1a8412d56c56575d451c83
Shiv <sky.shivy@gmail.com>
2024-08-24 22:33:17 +0530

    new

--- lib/files/screens/home_screen/home_screen.dart
+++ lib/files/screens/home_screen/home_screen.dart
@@ -1,35 +1,15 @@
-import 'dart:convert';
-
-import 'package:etisalat/files/api_calls/get_my_music_box_api.dart';
-import 'package:etisalat/files/api_calls/get_my_tune_api.dart';
-import 'package:etisalat/files/api_calls/get_playing_tune_api.dart';
-import 'package:etisalat/files/controllers/auth_controller/login_controller.dart';
-import 'package:etisalat/files/controllers/music_box_controller.dart';
-import 'package:etisalat/files/enums/fonts.dart';
-import 'package:etisalat/files/popup_views/gift_popup_view.dart';
-import 'package:etisalat/files/reusable_widgets/buttons/generic_button.dart';
-import 'package:etisalat/files/reusable_widgets/custom_alert_popup.dart';
-import 'package:etisalat/files/reusable_widgets/custom_textfield.dart';
-import 'package:etisalat/files/reusable_widgets/print_custom.dart';
-import 'package:etisalat/files/reusable_widgets/custom_text.dart';
-import 'package:etisalat/files/reusable_widgets/time_date_picker.dart';
-import 'package:etisalat/files/screens/authentication_screen/login_otp_popup.dart';
-import 'package:etisalat/files/screens/authentication_screen/login_popup.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:etisalat/files/utility/colors.dart';
 import 'package:etisalat/files/banners/bottombannerview.dart';
 import 'package:etisalat/files/banners/expressbannerview.dart';
-import 'package:etisalat/files/screens/home_screen/diy_screen.dart';
-import 'package:etisalat/files/screens/home_screen/remainder_screen.dart';
+import 'package:etisalat/files/store_manager/store_manager.dart';
+import 'package:etisalat/files/reusable_widgets/print_custom.dart';
+import 'package:etisalat/files/controllers/music_box_controller.dart';
+import 'package:etisalat/files/reusable_widgets/buttons/generic_button.dart';
+import 'package:etisalat/files/screens/home_screen/widget/music_box_view.dart';
 import 'package:etisalat/files/screens/home_screen/widget/home_banner_view/home_banner_view.dart';
 import 'package:etisalat/files/screens/home_screen/widget/feature_view/feature_category_view.dart';
-import 'package:etisalat/files/screens/home_screen/widget/music_box_view.dart';
-import 'package:etisalat/files/screens/my_tune_screen/my_tune_screen.dart';
-import 'package:etisalat/files/screens/profile_screen/profile_screen.dart';
-import 'package:etisalat/files/screens/web_navigation_view/web_navigation_view.dart';
-import 'package:etisalat/files/store_manager/store_manager.dart';
-import 'package:etisalat/files/utility/colors.dart';
-import 'package:etisalat/files/utility/strings.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -39,7 +19,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  LoginController loginController = Get.find();
   MusicBoxController musicBoxController = Get.find();
   DateTime p0 = DateTime.now();
   @override
@@ -78,35 +57,6 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          GenericButton(
-            title: " Open Time",
-            onTap: () {
-              openAlertPopup(
-                heading: "Heading",
-                message:
-                    "https://ringtune.mpt.com.mm/apigw/Middleware/api/adapter/v1/crbt/settings",
-                onPrimary: () {
-                  print("Primary call back");
-                },
-                onSecondry: () {
-                  print("Secondry call back");
-                },
-              );
-            },
-          ),
-          GenericButton(
-            title: " Open Calender",
-            onTap: () {
-              Get.dialog(
-                  barrierDismissible: true,
-                  TimeDatePicker(
-                    dateTime: p0,
-                    onConfirm: (p0) {
-                      this.p0 = p0;
-                    },
-                  ));
-            },
-          ),
           GenericButton(
             title: "Login",
             onTap: () {
