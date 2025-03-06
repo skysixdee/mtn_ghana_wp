937fba251c3b679a19d00480e5b72124aff75a4f
GokulHari0054 <gokul.hari@6dtech.co.in>
2025-03-06 11:04:06 +0530

    privacypolicyRemovedandPlayButtonBugFixed

--- lib/files/controllers/my_tune_controllers/my_playing_tune_controller.dart
+++ lib/files/controllers/my_tune_controllers/my_playing_tune_controller.dart
@@ -22,7 +22,7 @@ class MyPlayingTuneController extends GetxController {
   RxString message = ''.obs;
   RxList<ToneDetail> tuneList = <ToneDetail>[].obs;
   RxBool isShuffleOn = false.obs;
-  List<TuneInfo> musicList = [];
+  List<ListToneApk> musicList = [];
   RxBool switchingShuffle = false.obs;
 
   getPlayingTune() async {
@@ -45,11 +45,11 @@ class MyPlayingTuneController extends GetxController {
     isLoading.value = false;
   }
 
-  Future<List<TuneInfo>> _getMusicBox() async {
-    List<TuneInfo> tuneList1 = [];
+  Future<List<ListToneApk>> _getMusicBox() async {
+    List<ListToneApk> tuneList1 = [];
     MyMusicBoxModel model = await getMyMusicBoxApi();
     if (model.statusCode == 'SC0000') {
-      tuneList1 = model.responseMap?.listToneApk?.first.toneDetails ?? [];
+      tuneList1 = model.responseMap?.listToneApk ?? [];
     }
     return tuneList1;
   }
--- lib/files/reusable_widgets/web_footer_view.dart
+++ lib/files/reusable_widgets/web_footer_view.dart
@@ -26,7 +26,7 @@ class WebFooterView extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(children: [privacyPolicyButton()]),
+               // Row(children: [privacyPolicyButton()]),
                 Row(children: [termsAndConditionButton(context)]),
                 const SizedBox(height: 8),
                 Container(height: 1, color: white),
