7f01b59a9ffc72e348567c9b8f7da1eb58cc708a
Shiv <sky.shivy@gmail.com>
2025-07-10 16:53:45 +0530

    new

--- lib/files/api_calls/get_category_api.dart
+++ lib/files/api_calls/get_category_api.dart
@@ -3,10 +3,13 @@ import 'dart:convert';
 import 'package:mtn_ghana_wp/files/model/category_model.dart';
 
 import 'package:mtn_ghana_wp/files/network_manager/network_manager.dart';
+import 'package:mtn_ghana_wp/files/utility/get_transaction_id.dart';
 
 import 'package:mtn_ghana_wp/files/utility/urls.dart';
 
 Future<CategoryModel> getCategoryScApi() async {
-  Map<String, dynamic> map = await NetworkManager().get(getCategoryListUrl);
+  Map<String, int> header = {"transId": getTransactionId()};
+  Map<String, dynamic> map =
+      await NetworkManager().get(getCategoryListUrl, addInHeader: [header]);
   return categoryModelFromJson(json.encode(map));
 }
