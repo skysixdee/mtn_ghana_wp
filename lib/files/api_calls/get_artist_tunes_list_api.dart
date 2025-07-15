687c340fa2daff91b13374b50ba0411e77290354
Shiv <sky.shivy@gmail.com>
2025-07-15 18:22:51 +0530

    new

--- lib/files/api_calls/get_artist_tunes_list_api.dart
+++ lib/files/api_calls/get_artist_tunes_list_api.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
 import 'package:mtn_ghana_wp/files/model/artist_tune_list_model.dart';
+import 'package:mtn_ghana_wp/files/model/search_result_model.dart';
 import 'package:mtn_ghana_wp/files/network_manager/network_manager.dart';
 import 'package:mtn_ghana_wp/files/store_manager/store_manager.dart';
 import 'package:mtn_ghana_wp/files/utility/constants.dart';
 import 'package:mtn_ghana_wp/files/utility/urls.dart';
 
-Future<ArtistTuneListModel> getArtistTuneListScApi(String key,
+Future<SearchResultModel> getArtistTuneListScApi(String key,
     {int pageNo = 0}) async {
   Map<String, dynamic> jsonData = {
     "sortBy": "OrderBy",
@@ -20,5 +21,6 @@ Future<ArtistTuneListModel> getArtistTuneListScApi(String key,
   Map<String, dynamic> map = await NetworkManager().post(advanceSearchUrl,
       jsonData:
           jsonData); //mockyapi:'https://run.mocky.io/v3/3c30486a-4291-4667-b575-ce6a66e3105b'
-  return artistTuneListModelFromJson(json.encode(map));
+  return searchResultModelFromJson(
+      json.encode(map)); //artistTuneListModelFromJson(json.encode(map));
 }
--- lib/files/controllers/artists_tune_controller.dart
+++ lib/files/controllers/artists_tune_controller.dart
@@ -1,5 +1,6 @@
 import 'package:mtn_ghana_wp/files/api_calls/get_artist_tunes_list_api.dart';
 import 'package:mtn_ghana_wp/files/model/artist_tune_list_model.dart';
+import 'package:mtn_ghana_wp/files/model/search_result_model.dart';
 import 'package:mtn_ghana_wp/files/model/tune_info.dart';
 import 'package:get/get.dart';
 
@@ -15,16 +16,16 @@ class ArtistsTuneController extends GetxController {
       return;
     }
     isLoading.value = true;
-    ArtistTuneListModel model = await getArtistTuneListScApi(key);
-    tuneList = model.responseMap?.searchList ?? [];
-    totalToneCount.value = model.responseMap?.totalCount ?? 0;
+    SearchResultModel model = await getArtistTuneListScApi(key);
+    tuneList = model.responseMap?.toneList ?? [];
+    //totalToneCount.value = model.responseMap?.totalCount ?? 0;
     isLoading.value = false;
   }
 
   loadMoreData(int index) async {
     isLoading.value = true;
-    ArtistTuneListModel model = await getArtistTuneListScApi(_key, pageNo: index);
-    tuneList = model.responseMap?.searchList ?? [];
+    SearchResultModel model = await getArtistTuneListScApi(_key, pageNo: index);
+    tuneList = model.responseMap?.toneList ?? [];
     isLoading.value = false;
   }
 }
