925110b0a77e06051a9ad7ba3e9da28e4d64c8ef
Shiv <sky.shivy@gmail.com>
2025-08-20 16:25:28 +0530

    new

--- lib/files/screens/music_box/music_box_screen.dart
+++ lib/files/screens/music_box/music_box_screen.dart
@@ -1,11 +1,10 @@
 import 'package:mtn_ghana_wp/files/controllers/music_box_controller.dart';
 import 'package:mtn_ghana_wp/files/reusable_widgets/custom_scroll_view/generic_scroll_view.dart';
-import 'package:mtn_ghana_wp/files/reusable_widgets/generic_grid_view.dart';
+
 import 'package:mtn_ghana_wp/files/reusable_widgets/get_navigation_view.dart';
-import 'package:mtn_ghana_wp/files/reusable_widgets/loading_indicator.dart';
+
 import 'package:mtn_ghana_wp/files/reusable_widgets/music_box_card.dart';
-import 'package:mtn_ghana_wp/files/reusable_widgets/navigation_header_view.dart';
-import 'package:mtn_ghana_wp/files/reusable_widgets/tune_card.dart';
+
 import 'package:mtn_ghana_wp/files/utility/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
